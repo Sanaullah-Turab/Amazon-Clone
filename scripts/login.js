09069bfa441d2ec6ba1c719776ab46b87cbba4ed
Sanaullah-Turab <sanaullahmalik2003@gmail.com>
2024-12-23 17:27:54 +0500

    fix login button

--- scripts/login.js
+++ scripts/login.js
@@ -1,11 +1,18 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+const container = document.getElementById("container");
+const registerBtn = document.getElementById("register");
+const loginBtn = document.getElementById("login");
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
+registerBtn.addEventListener("click", () => {
+  container.classList.add("active");
 });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
+loginBtn.addEventListener("click", () => {
+  container.classList.remove("active");
 });
+
+document
+  .getElementById("signInButton")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+    window.location.href = "amazon.html"; // Redirect to amazon.html
+  });
